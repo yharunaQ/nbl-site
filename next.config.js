727cc23281c7a8f0eb5c1f7010508648b1fc15a9
Yuichiro Haruna <info@nextbeinglab.org>
2025-10-03 21:16:01 +0900

    chore: add /blog rewrite to blog.nextbeinglab.org

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const nextConfig = {
   async rewrites() {
     return [
       { source: '/docs/:path*', destination: 'https://files.nextbeinglab.org/:path*' },
+      { source: '/blog/:path*', destination: 'https://blog.nextbeinglab.org/:path*' },
     ];
   },
+  images: { formats: ['image/avif', 'image/webp'] },
 };
 module.exports = nextConfig;
